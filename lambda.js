71a7485f69a1014b260c29d1a8361df76f5c16b8
Adam Biglow <adam_biglow@acl.com>
2017-04-20 11:46:16 -0700

    hello world to slack

--- lambda.js
+++ lambda.js
@@ -1,10 +1,31 @@
-var lib = require('./lib/example.js');
+var http = require('http');
+var aws = require('aws-sdk');
+
+// config containing our slash command api token
+var config = require('./config.json');
 
 // Entrypoint for AWS Lambda
 exports.handler = function(event, context) {
+ var payloadData = {
+    channel: "#general",
+    username: "foosbot",
+    icon_emoji: ":soccer:",
+    text: 'hello world',
+  };
+  var payload = 'payload=' + JSON.stringify(payloadData);
+  var options = {
+    port: 443,
+    host: 'hooks.slack.com',
+    path: '/services/T52JDC3HU/B51JP3TRP/qeuwVmHahGEy9v2nX3MzyHc9',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': payload.length
+    }
+  };
 
-  if(event.type) {
-    return context.succeed(lib.getMessage(event.type));
-  }
-  return context.fail('No type provided!');
+  var req = http.request(options);
+
+  req.write(payload);
+  req.end();
 };
