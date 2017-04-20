a962118b77499cb02760259b7feacc8884e650ff
Adam Biglow <adam_biglow@acl.com>
2017-04-20 11:43:59 -0700

    init commit

--- lambda.js
+++ lambda.js
@@ -2,6 +2,7 @@ var lib = require('./lib/example.js');
 
 // Entrypoint for AWS Lambda
 exports.handler = function(event, context) {
+
   if(event.type) {
     return context.succeed(lib.getMessage(event.type));
   }
